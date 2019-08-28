10a9e65e144e5d22cb910db25d672067fb4da3d7
Ubuntu <ubuntu@ip-172-31-93-20.ec2.internal>
2019-08-28 12:21:58 +0000

    Change typing-error and try other lat and long

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,7 +1,7 @@
 function initMap() {
             var map = new google.maps.Map(document.getElementById("map"), {
                 zoom: 3,
-                center: {lat: 46.619261, lng: -33.13476}
+                center: {lat: 46.619261, lng: -33.13476} 
             });
             
            var labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
