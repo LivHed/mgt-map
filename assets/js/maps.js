c8a683dd95f181b1336a11e20310adb60fd7350f
Ubuntu <ubuntu@ip-172-31-93-20.ec2.internal>
2019-08-29 14:21:44 +0000

    Change lat,lng and zoom, try and comment out unnecessary code

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,13 +1,13 @@
-function initMap() {
+/* function initMap() {
             var map = new google.maps.Map(document.getElementById("map"), {
-                zoom: 3,
+                zoom: 12,
                 center: {lat: 46.619261, lng: -33.13476} 
             });
             
            var labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
            
-           var locations = [{     /*this map method here is a built-in JavaScript method. Don't get it confused with the Google Map that we're working with. comment from the teacher in the video*/
-                lat: 40.785091,
+       /*    var locations = [{     /*this map method here is a built-in JavaScript method. Don't get it confused with the Google Map that we're working with. comment from the teacher in the video*/
+         /*       lat: 40.785091,
                 lng: -73.968285
             }, {
                 lat: 41.084045,
@@ -15,7 +15,8 @@ function initMap() {
             }, {
                 lat: 40.754932,
                 lng: -73.984016
-            }];          /*Example of places, add others later*/
+            }];         /*Example of places, add others later*/ */
+            
             
              var markers = locations.map(function(location, i) {  /*check out lesson 5 in google maps section, to understand the details and adjust for my needs*/
                 return new google.maps.Marker({
