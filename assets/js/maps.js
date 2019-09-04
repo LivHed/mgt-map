a95ddd32d2ab455b3b0846f28ac71074685b4757
Ubuntu <ubuntu@ip-172-31-93-20.ec2.internal>
2019-09-04 14:33:34 +0000

    Remove outcommented code, change zoomlevel for a couple of cities

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -20,7 +20,7 @@ function initMap() {
         'frankfurt': [50.110924, 8.682127]
     }
 
-    /* Target ciities for zoom in on them when clicked on in the dropdown menu  */
+    /* Target cities to zoom in on them when clicked on, in the dropdown menu  */
     var city = document.getElementById("city");
     city.addEventListener("change", function() {
         console.log(city.value)
@@ -28,61 +28,61 @@ function initMap() {
         if (city.value == "london") {
             console.log(locations.london);
             map.setCenter(new google.maps.LatLng(locations.london[0], locations.london[1]));
-            map.setZoom(9);
+            map.setZoom(10);
         }
 
         if (city.value == "stockholm") {
             console.log(locations.stockholm);
             map.setCenter(new google.maps.LatLng(locations.stockholm[0], locations.stockholm[1]));
-            map.setZoom(9);
+            map.setZoom(10);
         }
         
         if (city.value == "edinburgh") {
             console.log(locations.edinburgh);
             map.setCenter(new google.maps.LatLng(locations.edinburgh[0], locations.edinburgh[1]));
-            map.setZoom(9);
+            map.setZoom(10);
         }    
         
         if (city.value == "singapore") {
             console.log(locations.singapore);
             map.setCenter(new google.maps.LatLng(locations.singapore[0], locations.singapore[1]));
-            map.setZoom(9);
+            map.setZoom(10);
         }
         
         if (city.value == "vienna") {
             console.log(locations.vienna);
             map.setCenter(new google.maps.LatLng(locations.vienna[0], locations.vienna[1]));
-            map.setZoom(9);
+            map.setZoom(10);
         }
         
         if (city.value == "zurich") {
             console.log(locations.zurich);
             map.setCenter(new google.maps.LatLng(locations.zurich[0], locations.zurich[1]));
-            map.setZoom(9);
+            map.setZoom(10);
         }
         
         if (city.value == "munich") {
             console.log(locations.munich);
             map.setCenter(new google.maps.LatLng(locations.munich[0], locations.munich[1]));
-            map.setZoom(9);
+            map.setZoom(10);
         }
         
          if (city.value == "oslo") {
             console.log(locations.oslo);
             map.setCenter(new google.maps.LatLng(locations.oslo[0], locations.oslo[1]));
-            map.setZoom(9);
+            map.setZoom(11);
         }
         
         if (city.value == "hongkong") {
             console.log(locations.hongkong);
             map.setCenter(new google.maps.LatLng(locations.hongkong[0], locations.hongkong[1]));
-            map.setZoom(9);
+            map.setZoom(11);
         }
         
         if (city.value == "frankfurt") {
             console.log(locations.frankfurt);
             map.setCenter(new google.maps.LatLng(locations.frankfurt[0], locations.frankfurt[1]));
-            map.setZoom(9);
+            map.setZoom(10);
         }
         
     });
@@ -118,10 +118,7 @@ function initMap() {
 
 
 
-
-
-
-/*         //Resets the map and all the input fields. remember to look if I have missed some input fields here..it does not mwork at the moment.  
+/*         //Resets the map and all the input fields. remember to look if I have missed some input fields here..it does not work at the moment.  
 function reset() {
  clearResults();
  $('#city')[0].selectedIndex = 0;
@@ -131,107 +128,3 @@ function reset() {
  place = "";
 }
          */
-
-
-
-
-
-
-// Use this if I want a Text search function with pre chosen words in a list.  
-
-/*  //Text search request          
-  var request = {
-    location: 'London',
-    radius: '500',
-    query: 'organic'
-  };
-
- var service = new google.maps.places.PlacesService(map);
-  service.textSearch(request, callback);
-  
-
-function callback(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      var place = results[i];
-      createMarker(results[i]);
-    }
-  }     */
-
-
-
-
-
-/* map.addListener('center_changed', function() {
-          // 3 seconds after the center of the map has changed, pan back to the
-          // marker.
-          window.setTimeout(function() {
-            map.panTo(marker.getPosition());
-          }, 3000);
-        }); */
-
-/*Listen for click on the map for zoom
-        marker.addListener(map, 'click', function(event) {
-          map.setZoom(8);
-          map.setCenter(marker.getPosition());
-        });
-
-
-/* // Set the country restriction based on user input.
-// Also center and zoom the map on the given country.
-function setAutocompleteCountry() {
- var city = $('#city').val();
- if (city == 'all') {
-  autocomplete.setComponentRestrictions({ 'city': [] });
-  map.setCenter({ lat: 15, lng: 0 });
-  map.setZoom(2);
- }
- else {
-  autocomplete.setComponentRestrictions({ 'city': city });
-  map.setCenter(cities[city].center);
-  map.setZoom(cities[city].zoom);
- }
-*/
-
-
-
-/*what I want to be returned in the PlaceResult (info window or in the list?) 
-formatted_adress 
-name
-types
-opening_hours
-
-  
-  
-         */
-
-
-/* copied from google maps javascript api documentation, places library for Text Search request */
-
-/*  function initialize() {
-  var london = new google.maps.LatLng(51.509865, -0.118092); */
-
-/*   map = new google.maps.Map(document.getElementById('map'), {
-     center: london,
-     zoom: 4
-   });  */
-
-/*  var request = {
-    location: London,
-    radius: '500',
-    query: 'organic'
-  };
-
-  service = new google.maps.places.PlacesService(map);
-  service.textSearch(request, callback);
-}
-
-function callback(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      var place = results[i];
-      createMarker(results[i]);
-    }
-  }
-}
-}*/
