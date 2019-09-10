ca1ac8ba79a833f7c29e0bf2951f236d109ebef7
Ubuntu <ubuntu@ip-172-31-93-20.ec2.internal>
2019-09-10 12:18:03 +0000

    comment out search function

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -199,16 +199,16 @@ function initMap() {
        } */
         }
         
-  /*      places.nearbySearch(search, function(results, status) {
+   /*     places.nearbySearch(search, function(results, status) {
                 if (status === google.maps.places.PlacesServiceStatus.OK) {
                     clearResults();
                     clearMarkers();    
                 }  
-                });
+                });   */
                 
                 // Create a marker for each hotel found, and
             // assign a letter of the alphabetic to each marker icon.
-            for (var i = 0; i < results.length; i++) {
+/*            for (var i = 0; i < results.length; i++) {
               var markerLetter = String.fromCharCode('A'.charCodeAt(0) + (i % 26));
               var markerIcon = MARKER_PATH + markerLetter + '.png';
               // Use marker animation to drop the icons incrementally on the map.
